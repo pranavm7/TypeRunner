9de4b50d1662bafc2b6b10c3f3fe19e8dafe080f
Pranav M <pranavm7@outlook.com>
2021-08-04 20:58:09 -0400

    Constructed Game object.

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,1 +1,21 @@
-console.log("hello world!");
+const config = {
+    type: Phaser.AUTO,
+    parent: 'game',
+    width: 800,
+    height: 640,
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: [MenuScene,],
+    physics: {
+        default: 'arcade',
+        arcade: { gravity: false}
+    },
+    pixelArt: true,
+};
+
+//  Game Settings can go here.
+// const gameSettings = {};
+
+const game = new Phaser.Game(config);
