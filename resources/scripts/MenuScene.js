4f5abf02bc0275b038614fd2037f3d54d0ca48ab
Annamalai Subramanian <subrama2@mail.uc.edu>
2021-08-09 18:50:36 -0400

    tying out a ways to add a button to menue

--- resources/scripts/MenuScene.js
+++ resources/scripts/MenuScene.js
@@ -2,20 +2,27 @@ class MenuScene extends Phaser.Scene{
     constructor() {
         super("menuScene");
     }
+    init(data){
+        console.log(data)
+        console.log("it works")
+    }
     preload(){
         //preloading the background buildings as images
-        this.load.image("Background", "resources/sprites/Background.png");
-   
+        this.load.image("background", "resources/sprites/Background.png");
 
+        
     }
 
-    create(){
-        var back = this.add.sprite(0,0, 'Background');
-        back.setOrigin(0,0);
-        var text = this.add.text(100,100, 'Welcome to my game!');
+    create() {
+        
+        
+        const helloButton = this.add.text(100, 100, 'Hello!', { fill: '#0f0' });
+        helloButton.setInteractive();
+        helloButton.on('pointerdown ', () => { console.log('pointerdown '); });
 
+    
         this.scene.start("bootGame");
-
+        
     }
    
 }
