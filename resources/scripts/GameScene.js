8f662fad3957b473ef55a00f6188e91bda7573a2
grigsbpk <84300601+grigsbpk@users.noreply.github.com>
2021-08-09 16:24:25 -0400

    added game animations

--- resources/scripts/GameScene.js
+++ resources/scripts/GameScene.js
@@ -9,6 +9,15 @@ class GameScene extends Phaser.Scene {
         this.background1.setOrigin(0,0);
         //adds sprite for moon
         this.background2 = this.add.sprite(config.width - 100, 120, "moon");
-        
+        //adds computerman
+        this.character = this.add.sprite(136, config.height/2, "character");
+        //adds corruption
+        this.corruption = this.add.sprite(32, config.height/2, "corruption");
+        //adds platform
+        this.platform1 = this.add.sprite(128, config.height/2 + 128, "platform1");
+
+        //adds animations
+        this.character.play("character_anim");
+        this.corruption.play("corruption_anim")
     }
 }
