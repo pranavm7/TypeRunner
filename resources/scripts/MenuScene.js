036b08ed13a77e31e86cfd6bf236e37a2497adb3
Annamalai Subramanian <subrama2@mail.uc.edu>
2021-08-09 14:23:56 -0400

    menuscreen not finished

--- resources/scripts/MenuScene.js
+++ resources/scripts/MenuScene.js
@@ -1,1 +1,21 @@
-class MenuScene extends Phaser.Scene{}
+class MenuScene extends Phaser.Scene{
+    constructor() {
+        super("menuScene");
+}
+preload(){
+    //preloading the background buildings as images
+    this.load.image("building1", "resources/sprites/Building.png");
+    //preloading the font
+    this.load.bitmapFont("pixelFont", "resources/font/font.png", "resources/font/font.xml");
+
+    }
+
+    create(){
+        var back = this.add.sprite(0,0, 'building1');
+        back.setOrgin(0,0);
+    
+        var text = this.add.text(100,100, 'Welcome To My Game!');
+    }
+   
+}
+
