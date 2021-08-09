d55092f062c9be8ccf346e096c253c2690ad82bf
grigsbpk <84300601+grigsbpk@users.noreply.github.com>
2021-08-09 15:22:30 -0400

    updated assets

--- resources/scripts/PreloadScene.js
+++ resources/scripts/PreloadScene.js
@@ -8,16 +8,16 @@ class PreloadScene extends Phaser.Scene{
         this.load.image("building1", "resources/sprites/Building.png");
         this.load.image("building2", "resources/sprites/Building2.png");
         this.load.image("building3", "resources/sprites/Building3.png");
-        this.load.image("building4", "resources/sprites/Chapel.png");
-        this.load.image("building5", "resources/sprites/Tower.png");
-        this.load.image("building6", "resources/sprites/Factory.png");
+        this.load.image("building4", "resources/sprites/Building4.png");
+        this.load.image("building5", "resources/sprites/Building5.png");
+        this.load.image("building6", "resources/sprites/Building6.png");
 
         //preloading the background assets
         this.load.image("background", "resources/sprites/Background.png");
         this.load.image("moon", "resources/sprites/Moon.png");
         this.load.spritesheet("corruption", "resources/sprites/Corruption.png", {
-            frameWidth: 16,
-            frameHeight: 256
+            frameWidth: 64,
+            frameHeight: 1024
         })
 
         //preloading the platforms as images
@@ -27,8 +27,8 @@ class PreloadScene extends Phaser.Scene{
 
         //preloading the character as a spritesheet
         this.load.spritesheet("character", "resources/sprites/Computerman-spritesheet.png", {
-            frameWidth: 34,
-            frameHeight: 32
+            frameWidth: 136,
+            frameHeight: 256
         });
 
         //preloading the font
