bcde39f89b2ced3399258707e05a4f1831b868a5
grigsbpk <84300601+grigsbpk@users.noreply.github.com>
2021-08-09 12:40:33 -0400

    preloaded images and spritesheets

---
+++ resources/scripts/PreloadScene.js
@@ -0,0 +1,24 @@
+class PreloadScene extends Phaser.Scene{
+
+    preload(){
+        //preloading the background buildings as images
+        this.load.image("building1", "resources/sprites/Building.png");
+        this.load.image("building2", "resources/sprites/Building2.png");
+        this.load.image("building3", "resources/sprites/Building3.png");
+        this.load.image("building4", "resources/sprites/Chapel.png");
+        this.load.image("building5", "resources/sprites/Tower.png");
+
+        //preloading the platforms as images
+        this.load.image("platform1", "resources/sprites/Platform1.png");
+        this.load.image("platform2", "resources/sprites/Platform2.png");
+        this.load.image("platform3", "resources/sprites/Platform3.png");
+
+        //preloading the character as a spritesheet
+        this.load.spritesheet("character", "resources/sprites/Computerman-spritesheet.png", {
+            frameWidth: 34,
+            frameHeight: 32
+        });
+        
+    }
+
+}
