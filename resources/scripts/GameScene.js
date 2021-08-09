f0c9eb99b714e75ef7d53eda8d34dd41c62b76a2
grigsbpk <84300601+grigsbpk@users.noreply.github.com>
2021-08-09 17:45:35 -0400

    added animations and black box for holding text

--- resources/scripts/GameScene.js
+++ resources/scripts/GameScene.js
@@ -6,16 +6,22 @@ class GameScene extends Phaser.Scene {
     create() {
         //adds tile for background
         this.background1 = this.add.tileSprite(0, 0, config.width, config.height, "background");
+        this.background1.tilePositionY = 150;
 
         //addsbuildings
-        //this.createBuildings();
+        // this.createBuildings();
         
-        //this.timer = this.time.addEvent({delay: 1500, callback: ()=>this.addBuildingFront, loop: true});
-        
+        //this.timer = this.time.addEvent({delay: 1500, callback: ()=>this.building1 = this.add.sprite(config.width, config.height - 256, "building1"), loop: true});
+        //this.building1 = this.add.sprite(config.width, config.height - 256, "building1")
 
         this.background1.setOrigin(0,0);
         //adds sprite for moon
         this.background2 = this.add.sprite(config.width - 100, 120, "moon");
+
+        //buildings
+        this.building2 = this.add.sprite(config.width, config.height - 240, "building2")
+        this.building1 = this.add.sprite(config.width, config.height - 300, "building1")
+        
         //adds platform
         this.platform = this.add.tileSprite(config.width/2, config.height/2 + 144, config.width, 32, "platform4");
         //adds computerman
@@ -40,9 +46,27 @@ class GameScene extends Phaser.Scene {
         graphics.lineTo(0, 0)
         graphics.closePath();
         graphics.fillPath();
+
+        var graphics2 = this.add.graphics();
+        graphics2.fillStyle(0x000000, 1);
+        graphics2.beginPath();
+        graphics2.moveTo(0, config.height);
+        graphics2.lineTo(config.width, config.height);
+        graphics2.lineTo(config.width, config.height-230);
+        graphics2.lineTo(0, config.height-230);
+        graphics2.lineTo(0, config.height);
+        graphics2.closePath();
+        graphics2.fillPath();
     }
 
     update() {
+        //animate background
+        this.background1.tilePositionX += .5;
+        //animate platform
+        this.platform.tilePositionX += 5;
+
+        this.moveBuilding(this.building1, 4);
+
 
     }
 
@@ -50,25 +74,25 @@ class GameScene extends Phaser.Scene {
     //     this.buildingsF = this.add.group();
     //     this.buildingsM = this.add.group();
     //     this.buildingsB = this.add.group();
-    //     //f
-    //     this.building1 = this.add.sprite(config.width, config.height - 256, "building1");
-    //     //b
-    //     this.building2 = this.add.sprite(config.width, config.height - 32, "building2");
-    //     //f
-    //     this.building3 = this.add.sprite(config.width, config.height - 256, "building3");
-    //     //m
-    //     this.building4 = this.add.sprite(config.width, config.height - 128, "building4");
-    //     //m
-    //     this.building5 = this.add.sprite(config.width, config.height - 128, "building5");
-    //     //m
-    //     this.building6 = this.add.sprite(config.width, config.height - 128, "building6");
+    //     // //f
+    //     // this.building1 = this.add.sprite(config.width, config.height - 256, "building1");
+    //     // //b
+    //     // this.building2 = this.add.sprite(config.width, config.height - 32, "building2");
+    //     // //f
+    //     // this.building3 = this.add.sprite(config.width, config.height - 256, "building3");
+    //     // //m
+    //     // this.building4 = this.add.sprite(config.width, config.height - 128, "building4");
+    //     // //m
+    //     // this.building5 = this.add.sprite(config.width, config.height - 128, "building5");
+    //     // //m
+    //     // this.building6 = this.add.sprite(config.width, config.height - 128, "building6");
 
-    //     this.buildingsF.add(this.building1);
-    //     this.buildingsF.add(this.building3);
-    //     this.buildingsB.add(this.building2);
-    //     this.buildingsM.add(this.building4);
-    //     this.buildingsM.add(this.building5);
-    //     this.buildingsM.add(this.building6);
+    //     this.buildingsF.add("building1");
+    //     this.buildingsF.add("building3");
+    //     this.buildingsB.add("building2");
+    //     this.buildingsM.add("building4");
+    //     this.buildingsM.add("building5");
+    //     this.buildingsM.add("building6");
     // }
 
     // addBuildingFront() {
@@ -82,4 +106,16 @@ class GameScene extends Phaser.Scene {
     //     this.randomBuildingF.outOfBoundsKill = true;
 
     // }
+
+    moveBuilding(building, speed) {
+        building.x -= speed;
+        if (building.x < -50) {
+            this.resetBuildingPos(building);
+        }
+    }
+
+    resetBuildingPos(building) {
+        building.x = config.width + (Math.random()%10 *1000);
+    }
+    
 }
